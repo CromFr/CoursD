9f42ce42bf0a36a1be0d57df3240d94af5dfcc4d
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-06-11 01:07:19 +0200

    Fixed cleaning of reveal.js before setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 VERSION=2.6.2
 
+if [ -d reveal.js ] ; then
+	rm -rf reveal.js
+fi
 wget https://github.com/hakimel/reveal.js/archive/${VERSION}.tar.gz
 tar xf ${VERSION}.tar.gz
 mv reveal.js-${VERSION} reveal.js
