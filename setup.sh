8fc982c41ad62c55c189d9c6a6676f5c45b872ee
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-08-27 16:56:01 +1000

    Modified setup.sh to keep downloaded reveal.js archive

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=2.6.2
 
 if [ -d reveal.js ] ; then
 	rm -rf reveal.js
 fi
-wget https://github.com/hakimel/reveal.js/archive/${VERSION}.tar.gz
+if [ -f ${VERSION}.tar.gz ] ; then
+	echo "Using previously downloaded reveal.js version"
+else
+	wget https://github.com/hakimel/reveal.js/archive/${VERSION}.tar.gz
+fi
+
 tar xf ${VERSION}.tar.gz
 mv reveal.js-${VERSION} reveal.js
-rm ${VERSION}.tar.gz
+
